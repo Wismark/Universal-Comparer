2ac125280006a459f35eb1ad3ee1223d2bb85d34
Nikolay Korotaev <nikolay.korotaev@dataart.com>
2019-03-13 17:54:55 +0200

    Add project files.

---
+++ UniversalComparer/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalComparer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var people = new List<Person>();
+            var person1 = new Person()
+            {
+                Born = DateTime.MinValue,
+                Chief = null,
+                FirstName = "John",
+                LastName = "Doe"
+            };
+            var person2 = new Person()
+            {
+                Born = DateTime.MinValue,
+                Chief = person1,
+                FirstName = "Donald",
+                LastName = "Duck"
+            };
+            var person3 = new Person()
+            {
+                Born = DateTime.MinValue,
+                Chief = person1,
+                FirstName = "Mike",
+                LastName = "Lobar"
+            };
+
+            var person4 = new Person()
+            {
+                Born = DateTime.MinValue,
+                Chief = person1,
+                FirstName = "Mike",
+                LastName = "Lobar"
+            };
+            people.AddRange(new Person[] { person1, person2, person3 });
+
+            string t1 = "LastName, FirstName, Born.Year desc", t2 = "FirstName desc, Born.Day, Chief.Born.Day desc",
+            t3 = "FirstName desc, Desc, Chief.Born.Day desc, descot desc", t4 = "LastName, FirstName"; //"FirstName desc, desc, Chief.Born.Day desc, descot desc";
+
+
+            //ShowCollection(people);
+            var comparer = new UniversalComparer(t4);
+            //people.Sort(0,3, comparer as IComparer<Person>);
+            //ShowCollection(people);
+
+            Console.WriteLine("--------");
+            Console.WriteLine(comparer.Test(person3, person4));
+            Console.ReadLine();
+        }
+
+        public static void ShowCollection(List<Person> peopleList)
+        {
+            Console.WriteLine("People:");
+            foreach (var person in peopleList)
+            {
+                Console.WriteLine(String.Concat($"Name:{person.FirstName} {person.LastName}  Born:{person.Born.Date.ToString("MM/dd/yyyy",CultureInfo.InvariantCulture)} "
+                                 ,(person.Chief is null ? "" : "Chief:"+ person.Chief.FirstName + " " + person.Chief.LastName)));
+            }
+        }
+        public class Person
+        {
+            public Person Chief;
+            public String FirstName;
+            public String LastName;
+            public DateTime Born;
+        }
+
+    }
+}
---
+++ UniversalComparer/UniversalComparer.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UniversalComparer
+{
+    class UniversalComparer : IComparer
+    {
+        private string SortString { get; set;}
+        private Type Type { get; set; }
+
+        public List<Condition> Conditions = new List<Condition>();
+
+        public UniversalComparer(string sortString)
+        {
+            SortString = sortString;
+            ParseSortCondition();
+        }
+
+        private void ParseSortCondition()
+        {
+            List<string> list = SortString.Split(' ').ToList();
+
+            //foreach (var s in list)
+            //{
+            //    Console.WriteLine(s);
+            //}
+
+            Console.WriteLine("--------");
+
+            for (int i=0; i<list.Count; i++)
+            {
+                Condition tempCondition = null;
+                if (list[i].Contains(','))
+                {
+                    list[i] = list[i].Remove(list[i].Length-1);
+                    if (list[i + 1].Contains("desc") && (!list[i].Contains("desc") || list[i].Length>4))
+                    {
+                        tempCondition = new Condition();
+                        tempCondition.desc=true;
+                        tempCondition.conditionParametr = list[i];
+                    }
+                    else
+                    {
+                        if (list[i].Length!=4)
+                        {
+                            tempCondition = new Condition();
+                            tempCondition.desc = false;
+                            tempCondition.conditionParametr = list[i];
+                        }
+                        else
+                        {
+                            if (!list[i].Contains("desc"))
+                            {
+                                tempCondition = new Condition();
+                                tempCondition.desc = false;
+                                tempCondition.conditionParametr = list[i];
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    if(!(i+1>list.Count-1))
+                    if (list[i + 1].Contains("desc"))
+                    {
+                        tempCondition = new Condition();
+                        tempCondition.desc = true;
+                        tempCondition.conditionParametr = list[i];
+                    }
+                }
+
+                if (list[i].Contains('.'))
+                {
+                  //  var tempStr = list[i].Split('.');
+                    if (list[i + 1].Contains("desc"))
+                    {
+                        tempCondition = new Condition();
+                        tempCondition.desc = true;
+                        tempCondition.conditionParametr = list[i];  //tempStr[tempStr.Length-1];
+                    }
+                };
+                if(tempCondition!=null) Conditions.Add(tempCondition);
+            }
+
+            foreach (var condition in Conditions)
+            {
+                Console.WriteLine(condition.conditionParametr + " " + condition.desc);
+            }
+
+        }
+
+        public int Test(object x, object y)
+        {
+            Type myType = x.GetType();
+            IList<FieldInfo> props = myType.GetFields();
+
+            string a;
+            //foreach (FieldInfo prop in props)
+            //{
+            //    Console.WriteLine(prop.GetValue(x) == null ? "null" : prop.GetValue(x));
+            //}
+            //Console.WriteLine("--------");
+
+            foreach (var condition in Conditions)
+            {
+                var value1 = (string)props.Single(p => p.Name == condition.conditionParametr).GetValue(x);
+                var value2 = (string)props.Single(p => p.Name == condition.conditionParametr).GetValue(y);
+
+                return String.Compare(value1, value2, StringComparison.Ordinal);
+            }
+
+            return 0;
+        }
+
+        public int Compare(object x, object y)
+        {
+            x.GetType().GetProperty("FirstName").GetValue(x);
+            throw new NotImplementedException();
+        }
+
+   
+    }
+
+    class Condition
+    {
+        public string conditionParametr;
+        public bool desc;
+    }
+}
